0a8686087324afd10f6d8f0e8c7599875fed42cd
Alexandra Ligacheva <alexandra-gall@mail.ru>
2020-03-06 00:57:17 +0300

    feat: add human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const digits = new Map([
+        [0, "zero"],
+        [1, "one"],
+        [2, "two"],
+        [3, "three"],
+        [4, "four"],
+        [5, "five"],
+        [6, "six"],
+        [7, "seven"],
+        [8, "eight"],
+        [9, "nine"],
+        [10, "ten"],
+        [11, "eleven"],
+        [12, "twelve"],
+        [13, "thirteen"],
+        [14, "fourteen"],
+        [15, "fifteen"],
+        [16, "sixteen"],
+        [17, "seventeen"],
+        [18, "eighteen"],
+        [19, "nineteen"],
+        [20, "twenty"],
+        [30, "thirty"],
+        [40, "forty"],
+        [50, "fifty"],
+        [60, "sixty"],
+        [70, "seventy"],
+        [80, "eighty"],
+        [90, "ninety"],
+        [100, "hundred"]
+    ]);
+    if (number < 20) {
+        return digits.get(number);
+    } else if (number < 100 && !(number % 10)) {
+        return digits.get(number);
+    } else if (number < 100) {
+        return number % 10 ?
+        `${digits.get(parseInt(number / 10) * 10)} ${digits.get(number % 10)}` :
+        digits.get(number / 10);
+    } else {
+        return number % 100 ?
+        `${digits.get(parseInt(number / 100))} ${digits.get(100)} ${toReadable(number % 100)}` :
+        `${digits.get(parseInt(number / 100))} ${digits.get(100)}`;
+    }
+};
